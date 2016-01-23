0276c9e3302944f1d7512e0e418dce396f0a48ec
jmortara <dev@mortaramultimedia.com>
2016-01-23 17:50:51 -0500

    2nd commit

--- app/src/main/java/com/mortaramultimedia/wordwolfappandroid/MainActivity.java
+++ app/src/main/java/com/mortaramultimedia/wordwolfappandroid/MainActivity.java
@@ -18,7 +18,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-
+/**
+ * Main Activity
+ * @author Jason Mortara
+ */
 public class MainActivity extends Activity {
 
     public static final String TAG = "WelcomeActivity";
